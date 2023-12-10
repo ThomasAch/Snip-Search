f3a2c878c71ed370c7e7c014df716089ad4e275d
ThomasAch <86896130+ThomasAch@users.noreply.github.com>
2023-12-09 23:47:45 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
-
+chrome.contextMenus.create({
+  id: "open-link",
+  title: "Open Link",
+  contexts: ["all"], // Show the item on any context
+  onclick: function() {
+    // Open a new tab in image search
+    chrome.tabs.create({url: ""});
+  }
+});
