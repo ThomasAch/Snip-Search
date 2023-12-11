825de39691908692f32583394ae27f8026217824
ThomasAch <86896130+ThomasAch@users.noreply.github.com>
2023-12-10 22:54:14 -0500

    Update background.js

--- background.js
+++ background.js
@@ -7,3 +7,16 @@ chrome.contextMenus.create({
     chrome.tabs.create({url: ""});
   }
 });
+
+// Add an event listener to the extension icon
+chrome.action.onClicked.addListener((tab) => {
+  // Capture the visible area of the current tab
+  chrome.tabs.captureVisibleTab(tab.windowId, { format: "png" }, (image) => {
+    // Create a blob object from the image data
+    let blob = new Blob([image], { type: "image/png" });
+    // Create a URL for the blob object
+    let url = URL.createObjectURL(blob);
+    // Download the image using the URL
+    chrome.downloads.download({ url: url, filename: "screenshot.png" });
+  });
+});
